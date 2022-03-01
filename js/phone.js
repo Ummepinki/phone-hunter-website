bdbc20677c3c66e8d515a9b7a5ce40b20f32ed02
Ummepinki <salmaumme888@gmail.com>
2022-03-01 20:19:03 +0600

    second added

--- js/phone.js
+++ js/phone.js
@@ -10,5 +10,25 @@ const searchPhone = () => {
 
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data));
+        .then(data => displaySearchResult(data.data));
+}
+const displaySearchResult = data => {
+    // console.log(data);
+    const searchResult = document.getElementById('search-result');
+    data.forEach(data => {
+        console.log(data);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+               <div class="card h-100">
+                   <img src="${data.image}" class="card-img-top" alt="...">
+                   <div class="card-body">
+                      <h5 class="card-title">${data.phone_name}</h5>
+                      <p class="card-text">${data.brand}</p>
+                 </div>
+           </div>
+       `;
+        searchResult.appendChild(div);
+    })
+
 }
